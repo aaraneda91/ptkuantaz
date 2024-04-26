49323dc49f9dc24ce756d1a87afa59ed35691268
Ariel Araneda <ariel.arzu@outlook.com>
2024-04-26 19:14:46 -0400

    primer commit

---
+++ database/migrations/2024_04_26_231151_create_fichas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fichas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',255);
+            $table->integer('id_programa')->nullable();
+            $table->string('url',255)->nullable();
+            $table->string('categoria',255)->nullable();
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fichas');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\FichaCollection;
 use Illuminate\Support\Facades\Route;
 
 /*
